a2c786aa9fbbe7db6ae27ca50a0270e8d0365aef
Ahmed Serag <ahmed@radivision.com>
2020-01-26 19:16:10 +0200

    feat: add new lines to content

--- source/ts/react-components/layouts/home/node.tsx
+++ source/ts/react-components/layouts/home/node.tsx
@@ -7,15 +7,17 @@
 import React from "react";
 import { Common } from "utilities/common";
 import { Node as NodeInterface } from "interfaces/node";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import CONTENT_ICON from "static/images/content-icon.png";
 import CLOSE_ICON from "static/images/close.png";
 
-export class Node extends React.Component<NodeInterface,{
-  isCollapsed: boolean;
-  isModalOpen: boolean;
-}> {
-  
+export class Node extends React.Component<
+  NodeInterface,
+  {
+    isCollapsed: boolean;
+    isModalOpen: boolean;
+  }
+> {
   constructor(props: NodeInterface) {
     super(props);
     this.state = {
@@ -26,7 +28,7 @@ export class Node extends React.Component<NodeInterface,{
     this.collapse = this.collapse.bind(this);
     this.triggerModal = this.triggerModal.bind(this);
   }
-  
+
   /**
    * Collapse node leaves.
    *
@@ -45,86 +47,84 @@ export class Node extends React.Component<NodeInterface,{
    * @memberof Node
    */
   triggerModal(action: boolean) {
-    if(this.state.isModalOpen !== action) {
+    if (this.state.isModalOpen !== action) {
       this.setState({
         isModalOpen: action
       });
     }
   }
 
-
   render(): React.ReactNode {
     return (
       <>
-      <li>
-        <a 
-          href={`#${this.props.title}`}
-          id={this.props.title}
-          onClick={this.collapse}
-        >
-          {this.props.title}
-        </a>
-        {Common.exist(this.props.content) && (
-            <img 
-              src={CONTENT_ICON} 
+        <li>
+          <a
+            href={`#${this.props.title}`}
+            id={this.props.title}
+            onClick={this.collapse}
+          >
+            {this.props.title}
+          </a>
+          {Common.exist(this.props.content) && (
+            <img
+              src={CONTENT_ICON}
               className="show-modal-btn"
-              onClick={()=>this.triggerModal(true)} 
+              onClick={() => this.triggerModal(true)}
             />
-        )}
-        {!this.state.isCollapsed && 
-          Common.exist(this.props.children) &&
-          (
+          )}
+          {!this.state.isCollapsed && Common.exist(this.props.children) && (
             <ul className="vertical">
-              {this.props.children.map((leaf, index)=> {
-                  return (
-                    <Node
-                      key={index}
-                      {...leaf}
-                    /> 
-                  );
+              {this.props.children.map((leaf, index) => {
+                return <Node key={index} {...leaf} />;
               })}
             </ul>
-          )
-        }
-      </li>
-      {Common.exist(this.props.content) && (
-        <Modal
-          isOpen={this.state.isModalOpen}
-          contentLabel={this.props.title}
-          ariaHideApp={false}
-        >
-          <img 
-            onClick={()=>this.triggerModal(false)}
-            className="close-modal"
-            src={CLOSE_ICON}
-          />
+          )}
+        </li>
+        {Common.exist(this.props.content) && (
+          <Modal
+            isOpen={this.state.isModalOpen}
+            contentLabel={this.props.title}
+            ariaHideApp={false}
+          >
+            <img
+              onClick={() => this.triggerModal(false)}
+              className="close-modal"
+              src={CLOSE_ICON}
+            />
             <h1>{this.props.title}</h1>
-            {this.props.content.map((block, i)=> {
+            {this.props.content.map((block, i) => {
               let response: React.ReactElement;
 
-              switch(block.type) {
+              switch (block.type) {
                 case "paragraph":
-                  response = <p key={i} >{block.value}</p>;
+                  response = <p key={i}>{(block.value as string).split("\n").map((line,index)=> {
+                    return(
+                      <>
+                      {line}
+                      <br/>
+                      </>
+                    );
+                  })}</p>;
                   break;
                 case "title":
-                  response = <h2 key={i} >{block.value}</h2>;
+                  response = <h2 key={i}>{block.value}</h2>;
                   break;
                 case "list":
                   response = (
                     <ul key={i}>
-                      {(block.value as string[]).map((item,index)=> {
-                        return <li key={index} >{item}</li>;
+                      {(block.value as string[]).map((item, index) => {
+                        return <li key={index}>{item}</li>;
                       })}
                     </ul>
                   );
                   break;
                 default:
-                  response = <p key={i} >{block.value}</p>;
+                  response = <p key={i}>{block.value}</p>;
               }
               return response;
             })}
-        </Modal>
-      )}
+          </Modal>
+        )}
       </>
     );
   }
